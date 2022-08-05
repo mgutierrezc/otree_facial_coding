d84c26057fae9bae089820f428c65be1b995cc07
Sweetdreamstome <greciabarandiaran@gmail.com>
2022-08-04 20:40:41 -0500

    save a .txt file each 300 pictures

--- _static/index.js
+++
@@ -1,7 +0,0 @@
-const files = require('fs');
-files.writeFile("/tmp/test", "Hey there!", function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-}); 
